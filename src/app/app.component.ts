3bcaec081c59df2f19a98de600b9746afc9caef5
ZengYing <vuuvv@qq.com>
2016-06-08 09:30:04 +0800

    angular2 webpack bundle start

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import '../styles.css';
+
+@Component({
+    selector: 'my-app',
+    template: require('./app.component.html'),
+    styles: [require('./app.component.css')]
+})
+
+export class AppComponent {
+    
+}
