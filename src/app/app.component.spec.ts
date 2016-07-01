2f1419681989e97b2f404c29d2051370c17ecfa1
ZengYing <vuuvv@qq.com>
2016-07-01 10:11:31 +0800

    fix fakeasync not working error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { it, inject, describe, beforeEachProviders, expect } from '@angular/core/testing';
+import { it, inject, fakeAsync, describe, beforeEachProviders, expect } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
@@ -10,4 +10,8 @@ describe('App', () => {
   it('should work', inject([AppComponent], (app: AppComponent) => {
     expect(2).toBe(2);
   }));
+
+  it('should pass', inject([AppComponent], fakeAsync((app: AppComponent) => {
+    expect(2).toBe(2);
+  })))
 })
